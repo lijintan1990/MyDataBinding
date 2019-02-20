ee742d94315c24a9c3fba1a8246fa5efaa4682e1
李金探 <lijintan@yunfan.com>
2019-02-20 15:22:42 +0800

    加注释

--- app/src/main/java/com/example/administrator/mydatabinding/User.java
+++ app/src/main/java/com/example/administrator/mydatabinding/User.java
@@ -3,6 +3,16 @@ import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.ObservableField;
 
+/**
+ * User类继承BaseObservable,可以实现真正的双向绑定，改变了数据，界面会跟着变化。
+ * 除此之外，还有一种更细粒度的绑定方式，可以具体到成员变量，这种方式无需继承 BaseObservable，一个简单的 POJO 就可以实现。
+
+ public class PlainUser {
+ public final ObservableField<String> firstName = new ObservableField<>();
+ public final ObservableField<String> lastName = new ObservableField<>();
+ public final ObservableInt age = new ObservableInt();
+ }
+ */
 public class User extends BaseObservable{
     private String firstName;
     private String lastName;
@@ -23,6 +33,10 @@ public class User extends BaseObservable{
         return firstName;
     }
 
+    /**
+     * @Bindable的作用是标记getter方法会在BR中生成一个元素
+     * @return
+     */
     @Bindable
     public String getLastName() {
         return lastName;
